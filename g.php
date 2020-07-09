8575cd3d1a77dbb38855db3bfb0fbf0b08bd2c7b
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 23:20:23 +0700

    Update g.php

--- g.php
+++ g.php
@@ -84,7 +84,7 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo "\n".color("purple"," ⏳▶️Please wait");
         for($a=1;$a<=3;$a++){
         echo color("nevy",".");
-        sleep(20);
+        sleep(5);
         }
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0607"}');
         $message = fetch_value($code1,'"message":"','"');
@@ -100,7 +100,7 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo color("nevy",".");
         sleep(10);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0607"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PAKEGOFOOD0607"}');
         $message = fetch_value($code1,'"message":"','"');
         echo "\n".color("purple"," Message: ".$message);
         echo "\n".color("nevy"," 🥂 CLAIM VOC D🥂.");
@@ -110,7 +110,7 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         sleep(7);
         }
         sleep(1);
-        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0607"}');
+        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PAKEGOFOOD0607"}');
         $messageboba09 = fetch_value($boba09,'"message":"','"');
         echo "\n".color("purple"," Message: ".$messageboba09);
         sleep(1);
