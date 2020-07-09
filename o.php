f1a14c07ca340c6330a400ed00450ba6b6ffbaa6
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 16:09:03 +0700

    Update o.php

--- o.php
+++ o.php
@@ -11,82 +11,23 @@ $header[] = 'X-Location: id_ID';
 $header[] ='Authorization: Bearer '.$token;
 $header[] = 'pin:'.$pin.'';
 // function change(){
-        echo color("purple","\n▬▬▬▬▬▬▬▬▬▬▬▬🔊AUTO CLAIM KAYANYA CUK🔊▬▬▬▬▬▬▬▬▬▬▬▬");
-        echo "\n".color("nevy"," 🥂CLAIM VOC A🥂.");
-        echo "\n".color("purple","⏳▶️ Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("nevy",".");
-        sleep(20);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("purple","Message: ".$message);
-        goto gocar;
-        }else{
-        echo "\n".color("purple"," Message: ".$message);
-	gocar:
-        echo "\n".color("nevy"," 🥂 CLAIM VOC B🥂. ");
-        echo "\n".color("purple"," ⏳▶️Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("nevy",".");
-        sleep(20);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2206"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){
-        echo "\n".color("purple","Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("purple"," Message: ".$message);
-        gofood:
-        echo "\n".color("nevy"," 🥂 CLAIM VOC C🥂.");
-        echo "\n".color("purple"," ⏳▶️Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("nevy",".");
-        sleep(10);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PAKEGOFOOD2206"}');
-        $message = fetch_value($code1,'"message":"','"');
-        echo "\n".color("purple"," Message: ".$message);
-        echo "\n".color("nevy"," 🥂 CLAIM VOC D🥂.");
-        echo "\n".color("purple"," ⏳▶️Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("nevy",".");
-        sleep(7);
-        }
-        sleep(1);
-        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
-        $messageboba09 = fetch_value($boba09,'"message":"','"');
-        echo "\n".color("purple"," Message: ".$messageboba09);
-        sleep(1);
-        $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=13&page=1', $token);
-        $total = fetch_value($cekvoucher,'"total_vouchers":',',');
-        $voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
-        $voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
-        $voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
-        $voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
-        $voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
-        $voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
-        $voucher7 = getStr1('"title":"','",',$cekvoucher,"7");
-        $voucher8 = getStr1('"title":"','",',$cekvoucher,"8");
-        $voucher9 = getStr1('"title":"','",',$cekvoucher,"9");
-        $voucher10 = getStr1('"title":"','",',$cekvoucher,"10");
-        $voucher11 = getStr1('"title":"','",',$cekvoucher,"11");
-        $voucher12 = getStr1('"title":"','",',$cekvoucher,"12");
-        $voucher13 = getStr1('"title":"','",',$cekvoucher,"13");
-        echo "\n".color("purple"," Total voucher ".$total." : ");
-        echo "\n".color("nevy","                     1. ".$voucher1);
-        echo "\n".color("purple","                     2. ".$voucher2);
-        echo "\n".color("nevy","                     3. ".$voucher3);
-        echo "\n".color("purple","                     4. ".$voucher4);
-        echo "\n".color("nevy","                     5. ".$voucher5);
-        echo "\n".color("purple","                     6. ".$voucher6);
-        echo "\n".color("nevy","                     7. ".$voucher7);
-        echo "\n".color("purple","                     8. ".$voucher8);
-        echo "\n".color("nevy","                     9. ".$voucher9);
-        echo "\n".color("purple","                     10. ".$voucher10);
-	echo "\n".color("nevy","                     11. ".$voucher11);
-        echo "\n".color("purple","                     12. ".$voucher12);
-        echo "\n".color("purple","                     13. ".$voucher13);
-        echo"\n";
+      else
+        {
+        file_put_contents("token/".$verif['data']['customer']['name'].".txt", $verif['data']['access_token']);
+        echo "\e[93m[X] Mencoba Redeem : PAKEGOFOOD\n";
+        sleep(3);
+        $claim = claim($verif);
+        if ($claim == false)
+            {
+            echo "\e[92m[!]".$voucher."\n";
+            sleep(3);
+            }
+            else{
+                echo "\e[92m[X] ".$claim."\n";
+                sleep(3);
+            }
+    }
+    }
+
+
+?>
