985c1e08a1173a02c5c75e4a4d6ba6bae57f58a0
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 21:46:12 +0700

    Update o.php

--- o.php
+++ o.php
@@ -57,4 +57,5 @@ $header[] = 'pin:'.$pin.'';
         echo "\n".color("nevy","                     1. ".$voucher1);
         echo "\n".color("purple","                     2. ".$voucher2);
 //  }
-?>
+
+// echo change()."\n";
