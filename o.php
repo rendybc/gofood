f4ad10925a35abecf7c323f031840fed2cc24f46
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 15:29:12 +0700

    Update o.php

--- o.php
+++ o.php
@@ -10,14 +10,6 @@ $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = 'X-Location: id_ID';
 $header[] ='Authorization: Bearer '.$token;
 $header[] = 'pin:'.$pin.'';
-//CHECKER DETAIL AKUN
-$info = curl('https://api.gojekapi.com/v1/chat/profile', null, $header);
-    $verifs = json_decode($info[0]);
-         $akun = $verifs->data->name;
-         $nope = $verifs->data->phone;
-   echo "\n";
-   echo color("yellow","nama : ".$akun." \n");
-   echo color("yellow","nomer : 0".$nope." \n");
 //CHECKER SALDO GOPAY
 $detail = curl('https://api.gojekapi.com/wallet/profile/detailed', null, $header);
          $saldoo = json_decode($detail[0]);
