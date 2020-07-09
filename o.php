b3186ae6f450f87d16ad59bb0cc7deefc505109a
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 19:08:36 +0700

    Update o.php

--- o.php
+++ o.php
@@ -1,5 +1,4 @@
 <?php
-echo color("green","[+] Token GOJEK Kamu Disini : ");
 $token = trim(fgets(STDIN));
 $secret = ''.$token.'';
 $header = array();
@@ -22,3 +21,45 @@ $detail = curl('https://api.gojekapi.com/wallet/profile/detailed', null, $header
          $saldoo = json_decode($detail[0]);
                 $saldo = $saldoo->data->balance;
                     echo color("yellow","Sisa Saldo Gopay = $saldo \n");
+// function change(){
+        $nama = nama();
+        $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
+        echo color("nevy","📲▶️ NOMOR LO CUK : ");
+        // $no = trim(fgets(STDIN));
+        $nohp = trim(fgets(STDIN));
+        $nohp = str_replace("62","62",$nohp);
+        $nohp = str_replace("(","",$nohp);
+        $nohp = str_replace(")","",$nohp);
+        $nohp = str_replace("-","",$nohp);
+        $nohp = str_replace(" ","",$nohp);
+
+        if (!preg_match('/[^+0-9]/', trim($nohp))) {
+            if (substr(trim($nohp),0,3)=='62') {
+                $hp = trim($nohp);
+            }
+            else if (substr(trim($nohp),0,1)=='0') {
+                $hp = '62'.substr(trim($nohp),1);
+        }
+         elseif(substr(trim($nohp), 0, 2)=='62'){
+            $hp = '6'.substr(trim($nohp), 1);
+        }
+        else{
+            $hp = '1'.substr(trim($nohp),0,13);
+        }
+    }
+        $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$hp.'","signed_up_country":"ID"}';
+        $login = request("/v5/customers", null, $data);
+        if(strpos($register, '"otp_token"')){
+        $otptoken = getStr('"otp_token":"','"',$login);
+        echo color("purple","📶▶️ KODE OTP UDAH GUA KIRIM CUK")."\n";
+        otp:
+        echo color("nevy","💬▶️ Otp : ");
+        $otp = trim(fgets(STDIN));
+        $data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
+        $verif = request("/v5/login/token/verify", null, $data1);
+        if(strpos($verif, '"access_token"')){
+        echo color("purple","✔️▶️ BERHASIL MEMDAFTAR\n");
+        $token = getStr('"access_token":"','"',$verif);
+        $uuid = getStr('"resource_owner_id":',',',$verif);
+        echo color("nevy","+] AKSES TOKEN LO : ".$token."\n\n");
+        save("token.txt",$token);
