b1477ccff5130dabf4db650914f4854aa942b5ec
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 15:22:14 +0700

    Update o.php

--- o.php
+++ o.php
@@ -11,15 +11,20 @@ $header[] = 'X-Location: id_ID';
 $header[] ='Authorization: Bearer '.$token;
 $header[] = 'pin:'.$pin.'';
 // function change(){
-function claim($token)
-    {
-    $data = '{"promo_code":"PAKEGOFOOD"}';    
-    $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
-    if ($claim['success'] == 1)
-        {
-        return $claim['data']['message'];
+        echo "\n".color("nevy"," 🥂CLAIM VOC A🥂.");
+        echo "\n".color("purple","⏳▶️ Please wait");
+        for($a=1;$a<=3;$a++){
+        echo color("nevy",".");
+        sleep(20);
         }
-      else
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
+        $message = fetch_value($code1,'"message":"','"');
+        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
+        echo "\n".color("purple","Message: ".$message);
+        goto gocar;
+        }else{
+        echo "\n".color("purple"," Message: ".$message);
+	gocar:
         {
       save("error_log.txt", json_encode($claim));
         return false;
