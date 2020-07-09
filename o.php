f55fc46ad92195704334f5f2ab71ba37261de7cb
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 21:29:04 +0700

    Update o.php

--- o.php
+++ o.php
@@ -26,51 +26,22 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
 // function change(){
         $nama = nama();
         $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
-        echo color("nevy","📲▶️ TOKEN : ");
-        // $no = trim(fgets(STDIN));
-        $nohp = trim(fgets(STDIN));
-        $nohp = str_replace("62","62",$nohp);
-        $nohp = str_replace("(","",$nohp);
-        $nohp = str_replace(")","",$nohp);
-        $nohp = str_replace("-","",$nohp);
-        $nohp = str_replace(" ","",$nohp);
-
-        if (!preg_match('/[^+0-9]/', trim($nohp))) {
-            if (substr(trim($nohp),0,3)=='62') {
-                $hp = trim($nohp);
-            }
-            else if (substr(trim($nohp),0,1)=='0') {
-                $hp = '62'.substr(trim($nohp),1);
-        }
-         elseif(substr(trim($nohp), 0, 2)=='62'){
-            $hp = '6'.substr(trim($nohp), 1);
-        }
-        else{
-            $hp = '1'.substr(trim($nohp),0,13);
-        }
-    }
-        $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$hp.'","signed_up_country":"ID"}';
-        $login = request("/v5/customers", null, $data);
-        if(strpos($login, '"otp_token"')){
-        $otptoken = getStr('"otp_token":"','"',$login);
-        echo color("purple","📶▶️ KODE OTP UDAH GUA KIRIM CUK")."\n";
-        otp:
-        echo color("nevy","💬▶️ Otp : ");
-        $otp = trim(fgets(STDIN));
-        $data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
-        $verif = request("/v5/customers/phone/verify", null, $data1);
-        if(strpos($verif, '"access_token"')){
-        echo color("purple","✔️▶️ BERHASIL MEMDAFTAR\n");
-        $token = getStr('"access_token":"','"',$verif);
-        $uuid = getStr('"resource_owner_id":',',',$verif);
-        echo color("nevy","+] AKSES TOKEN LO : ".$token."\n\n");
-        save("token.txt",$token);
+        echo color("green","[+] Token GOJEK Kamu Disini : ");
+$token = trim(fgets(STDIN));
+$secret = ''.$token.'';
+$header = array();
+$header[] = 'Content-Type: application/json';
+$header[] = 'X-AppVersion: 3.46.2';
+$header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
+$header[] = 'X-Location: id_ID';
+$header[] ='Authorization: Bearer '.$token;
+$header[] = 'pin:'.$pin.'';
         echo color("purple","\n▬▬▬▬▬▬▬▬▬▬▬▬🔊AUTO CLAIM KAYANYA CUK🔊▬▬▬▬▬▬▬▬▬▬▬▬");
         echo "\n".color("nevy"," 🥂CLAIM VOC A🥂.");
         echo "\n".color("purple","⏳▶️ Please wait");
         for($a=1;$a<=3;$a++){
         echo color("nevy",".");
-        sleep(20);
+        sleep(3);
         }
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
         $message = fetch_value($code1,'"message":"','"');
@@ -84,7 +55,7 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo "\n".color("purple"," ⏳▶️Please wait");
         for($a=1;$a<=3;$a++){
         echo color("nevy",".");
-        sleep(20);
+        sleep(3);
         }
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2206"}');
         $message = fetch_value($code1,'"message":"','"');
@@ -98,7 +69,7 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo "\n".color("purple"," ⏳▶️Please wait");
         for($a=1;$a<=3;$a++){
         echo color("nevy",".");
-        sleep(10);
+        sleep(3);
         }
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PAKEGOFOOD2206"}');
         $message = fetch_value($code1,'"message":"','"');
@@ -107,13 +78,13 @@ echo color("purple","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         echo "\n".color("purple"," ⏳▶️Please wait");
         for($a=1;$a<=3;$a++){
         echo color("nevy",".");
-        sleep(7);
+        sleep(3);
         }
-        sleep(1);
+        sleep(3);
         $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
         $messageboba09 = fetch_value($boba09,'"message":"','"');
         echo "\n".color("purple"," Message: ".$messageboba09);
-        sleep(1);
+        sleep(3);
         $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=13&page=1', $token);
         $total = fetch_value($cekvoucher,'"total_vouchers":',',');
         $voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
