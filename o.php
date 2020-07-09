8133954fa8591edee99d8564ceb5b9dd12e66f43
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 15:52:18 +0700

    Update o.php

--- o.php
+++ o.php
@@ -1,9 +1,9 @@
 <?php
 include "rendygans.php";
 echo color("green","[+] Token GOJEK Kamu Disini : ");
-$token = trim(fgets(STDIN));
-$secret = ''.$token.'';
-$header = array();
+$number = trim(fgets(STDIN));
+$secret = 'xxxx-xx-xxx-xxxx-xxxxxx'; //TOKEN AKUN VERIF GOPAY LO
+$headers = array();
 $header[] = 'Content-Type: application/json';
 $header[] = 'X-AppVersion: 3.46.2';
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
