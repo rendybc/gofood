3ea06547b99d2b1cb62c57c07091fdf3f1f5b342
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-09 12:20:20 +0700

    Create o.php

---
+++ o.php
@@ -0,0 +1,11 @@
+<?php
+echo color("green","[+] Token GOJEK Kamu Disini : ");
+$token = trim(fgets(STDIN));
+$secret = ''.$token.'';
+$header = array();
+$header[] = 'Content-Type: application/json';
+$header[] = 'X-AppVersion: 3.46.2';
+$header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
+$header[] = 'X-Location: id_ID';
+$header[] ='Authorization: Bearer '.$token;
+$header[] = 'pin:'.$pin.'';
