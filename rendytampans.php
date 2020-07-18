ba27cf1313dc1b70e1dee6a1c0e44c3b3b9066a8
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-19 00:47:03 +0700

    Update rendytampans.php

--- rendytampans.php
+++ rendytampans.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.301664,106.879584);
+$header[] = "X-Location: -5.148411,119.432828";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
