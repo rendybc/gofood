c92d651ee908b76a1aeac18fc512b98e5e00e201
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-18 23:41:23 +0700

    Update sgbvoc1.php

--- sgbvoc1.php
+++ sgbvoc1.php
@@ -123,7 +123,6 @@ echo color("white","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
          echo color("yellow","!] Input ulang nomor mu a n");
          goto otp;
          }
-         else{
          }
          echo color("red","-] NOMOR SALAH/ERROR BY SYSTEM");
          echo"\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n";
