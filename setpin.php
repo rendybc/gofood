9a3b7eacc3827d41d6471db3145184260bb8390d
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-25 20:00:24 +0700

    Update setpin.php

--- setpin.php
+++ setpin.php
@@ -41,11 +41,7 @@ ulang:
          $otpsetpin = trim(fgets(STDIN));
          $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
          echo $verifotpsetpin;
-         }else if($pilih1 == "n" || $pilih1 == "N"){
-         die();
-         }else{
          echo color("green","-] Done\n");
-	 echo color("white","\e[61m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n");
 	 echo "\n";
          }
          }
