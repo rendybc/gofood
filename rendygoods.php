f4ab51e3d6cb3cfd9f837d596a1b0566205152f8
rendybc <67693618+rendybc@users.noreply.github.com>
2020-07-25 18:01:37 +0700

    Update rendygoods.php

--- rendygoods.php
+++ rendygoods.php
@@ -49,6 +49,24 @@ function save($filename, $content)
     fputs($save, "$content\r\n");
     fclose($save);
 }
+function curl($url, $fields = null, $headers = null) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  if ($fields !== null) {
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+  }
+  if ($headers !== null) {
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  }
+  $result = curl_exec($ch);
+  $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+  return array($result, $httpcode);
+  }
 function nama()
     {
     $ch = curl_init();
