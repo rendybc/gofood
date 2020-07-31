db644290d0214f33e7cfa7eda559bde3da3e253f
Bananacreamy <67693618+rendybc@users.noreply.github.com>
2020-07-31 16:35:33 +0700

    Update rendygans.php

--- rendygans.php
+++ rendygans.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.56.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.919081,107.618661";
+$header[] = "X-Location: -6.268066,106.870237";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
